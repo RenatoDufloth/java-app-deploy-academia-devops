26ca31d5520e06fc73749fe799dfefa55fbc05ae
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-18 14:59:03 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,10 +11,18 @@ provider "aws" {
 	resource "aws_instance" "maquina_nodejs" {
 	  ami           = "ami-09e67e426f25ce0d7"
 	  instance_type = "t2.micro"
-	  key_name      = "treinamento-turma1_itau"
+	  key_name      = "key-dufloth-devout"
 	  tags = {
-	    Name = "maquina_ansible_com_nginx_e_nodejs"
+	    Name = "dufloth-maquina_ansible_com_nginx_e_nodejs"
 	  }
+	   subnet_id                   = "subnet-05cdfe4fe6a3d1c13"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = ["${aws_security_group.allow_ssh.id}", "${aws_security_group.allow_80.id}"]
+  root_block_device {
+    encrypted = true
+    #kms_key_id  = "arn:aws:kms:us-east-1:534566538491:key/90847cc8-47e8-4a75-8a69-2dae39f0cc0d"
+    volume_size = 20
+  }	
 	  vpc_security_group_ids = [aws_security_group.acessos_nodejs.id]
 	}
 	
@@ -77,7 +85,7 @@ provider "aws" {
 	
 
 	  tags = {
-	    Name = "allow_ssh"
+	    Name = "allow_ssh_dufloth"
 	  }
 	}
 	
