e80f9c5e42aac25d073a5c5215f41e73f7ac09f1
Renato Dufloth <renatodufloth@hotmail.com>
2021-10-18 18:12:42 +0000

    vpc

---
+++ terraform/vpc.tf
@@ -0,0 +1,96 @@
+
+resource "aws_security_group" "allow_ssh" {
+	  name        = "allow_ssh"
+	  description = "Allow ssh inbound traffic"
+	  vpc_id      = "vpc-0404e2502328d5e45"
+	
+
+	
+
+	  ingress = [
+	    {
+	      description      = "ssh from VPC"
+	      from_port        = 22
+	      to_port          = 22
+	      protocol         = "tcp"
+	      cidr_blocks      = ["0.0.0.0/0"]
+	      ipv6_cidr_blocks = ["::/0"]
+	      prefix_list_ids  = null
+	      security_groups : null
+	      self : null
+	    }
+	
+
+	  ]
+	
+
+	  egress = [
+	    {
+	      description      = "ssh from VPC"
+	      from_port        = 0
+	      to_port          = 0
+	      protocol         = "-1"
+	      cidr_blocks      = ["0.0.0.0/0"]
+	      ipv6_cidr_blocks = ["::/0"]
+	      prefix_list_ids  = null
+	      security_groups : null
+	      self : null
+	    }
+	  ]
+	
+
+	
+
+	  tags = {
+	    Name = "allow_ssh"
+	  }
+	}
+	resource "aws_security_group" "allow_80" {
+	  name        = "allow_80"
+	  description = "Allow 80 inbound traffic"
+	  vpc_id      = "vpc-0404e2502328d5e45"
+	
+
+	
+
+	  ingress = [
+	    {
+	      description      = "80 from VPC"
+	      from_port        = 80
+	      to_port          = 80
+	      protocol         = "tcp"
+	      cidr_blocks      = ["0.0.0.0/0"]
+	      ipv6_cidr_blocks = ["::/0"]
+	      prefix_list_ids  = null
+	      security_groups : null
+	      self : null
+	    }
+	
+
+	  ]
+	
+
+	  egress = [
+	    {
+	      description      = "80 from VPC"
+	      from_port        = 0
+	      to_port          = 0
+	      protocol         = "-1"
+	      cidr_blocks      = ["0.0.0.0/0"]
+	      ipv6_cidr_blocks = ["::/0"]
+	      prefix_list_ids  = null
+	      security_groups : null
+	      self : null
+	    }
+	  ]
+	
+
+	
+
+	  tags = {
+	    Name = "allow_80"
+	  }
+	}
+
+
+
