76ff53e0330a98fab7d7d14e70a63c82669fb1dd
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-18 15:16:24 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,68 +28,6 @@ resource "aws_instance" "maquina_nodejs" {
 }
 
 
-resource "aws_security_group" "acessos_nodejs" {
-  name        = "acessos_nodejs"
-  description = "acessos inbound traffic"
-
-
-  ingress = [
-    {
-      description      = "SSH from VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = null,
-      security_groups : null,
-      self : null
-    },
-    {
-      description      = "Acesso HTTPS"
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = null,
-      security_groups : null,
-      self : null
-    },
-    {
-      description      = "Acesso HTTP"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = null,
-      security_groups : null,
-      self : null
-    }
-  ]
-
-
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"],
-      prefix_list_ids  = null,
-      security_groups : null,
-      self : null,
-      description : "Libera dados da rede interna"
-    }
-  ]
-
-
-  tags = {
-    Name = "allow_ssh_dufloth"
-  }
-}
-
 
 output "aws_instance_e_ssh" {
   value = aws_instance.maquina_nodejs.public_dns
